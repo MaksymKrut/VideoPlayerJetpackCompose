79475f31a99fc6ba7de0e5a57778507b33968c4c
Maksym Krutskykh <maximkrut@yahoo.com>
2022-09-25 20:31:07 -0400

    Wireframe - part 4

--- app/src/main/java/com/silverorange/videoplayer/MainView.kt
+++ app/src/main/java/com/silverorange/videoplayer/MainView.kt
@@ -1,5 +1,6 @@
 package com.silverorange.videoplayer
 
+import android.net.Uri
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +10,13 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.ui.StyledPlayerView
+import dev.jeziellago.compose.markdowntext.MarkdownText
 import org.json.JSONObject
 
 @Composable
@@ -42,11 +50,31 @@ fun VideoScreen(mainViewModel: MainViewModel) {
 
 @Composable
 fun VideoPlayer(currentVideoObject: JSONObject) {
-    var currentAuthorObject = currentVideoObject.getJSONObject("author")
-    Text(text = "Current video name: ${currentAuthorObject["name"]}")
+    val localContext = LocalContext.current
+    val mExoPlayer = remember(localContext) {
+        ExoPlayer.Builder(localContext).build().apply {
+            val mediaItem = MediaItem.Builder()
+                .setUri(Uri.parse(currentVideoObject["hlsURL"] as String))
+                .build()
+            setMediaItem(mediaItem)
+            playWhenReady = true
+            prepare()
+        }
+    }
+    AndroidView(factory = { context ->
+        StyledPlayerView(context).apply {
+            player = mExoPlayer
+        }
+    })
 }
 
 @Composable
 fun VideoDescription(currentVideoObject: JSONObject) {
-    Text(text = "Current video title: ${currentVideoObject["title"]}")
+    Text(text = "Title: ${currentVideoObject["title"]}\n")
+    var currentAuthorObject = currentVideoObject.getJSONObject("author")
+    Text(text = "Author: ${currentAuthorObject["name"]}\n")
+    MarkdownText(
+        markdown = currentVideoObject["description"].toString(),
+        fontSize = 20.sp
+    )
 }
